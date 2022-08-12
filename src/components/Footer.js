1d40d604524756cece54dfe1c9d231ac41f6ad59
Rajesha T <rajesha@wittypen.com>
2022-08-12 12:22:29 +0530

    :rocket:

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        Coded with ❤️ by{" "}
+        <a
+          className="footer--link"
+          href="https://rajesha.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rajesha T
+        </a>
+        .
+      </small>
+    </footer>
+  );
+}
